9441bb62a6d1ea3572a46aa63291535431496c57
kjy0349 <41959969+kjy0349@users.noreply.github.com>
2024-06-13 18:26:08 +0900

    Time: 0 ms (100.00%), Space: 41 MB (68.83%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,54 +1,54 @@
 import java.io.*;
 import java.util.*;
 class Solution {
-    private int getDiffCount(int[] target, int[] sorted) throws IllegalArgumentException{
-        if (target.length != sorted.length) {
-            throw new IllegalArgumentException("Invalid Input for arrays.");
-        }
-        int diffCount = 0;
-        for (int i = 0; i < target.length; i++) {
-            if (target[i] != sorted[i]) {
-                diffCount++;
-            }
-        }
-        return diffCount;
+    static int answer;
+    static int[] original;
+    
+    private void swap(int[] array, int first, int second) {
+        int temp = array[second];
+        array[second] = array[first];
+        array[first] = temp;
     }
     
-//     private void swap(int[] array, int first, int second) {
-//         int temp = array[second];
-//         array[second] = array[first];
-//         array[first] = temp;
-//     }
-    
-//     private void quickSort(int[] sorted, int leftIdx, int rightIdx) {
-//         if (leftIdx >= rightIdx) {
-//             return ;
-//         }
-//         int pivot = leftIdx;
-//         int left = leftIdx;
-//         int right = rightIdx;
-//         while (left < right) {
-//             while (sorted[left] < sorted[pivot]) {
-//                 left++;
-//             }
-//             while (sorted[right] > sorted[pivot]) {
-//                 right--;
-//             }
-//             if (left < right) {
-//                 swap(sorted, left, right);
-//             }
-//         }
-//         swap(sorted, pivot, right);
-//         quickSort(sorted, leftIdx, pivot - 1);
-//         quickSort(sorted, pivot + 1, rightIdx);
-//     }
+    private void quickSort(int[] sorted, int leftIdx, int rightIdx) {
+        if (leftIdx >= rightIdx) {
+            if (leftIdx == rightIdx) {
+                if (sorted[leftIdx] != original[leftIdx]) {
+                    answer++;
+                }
+            }
+            return ;
+        }
+        int pivot = sorted[leftIdx];
+        int left = leftIdx + 1;
+        int right = rightIdx;
+        
+        while (left <= right) {
+            while (left <= right && sorted[left] < pivot) {
+                left++;
+            }
+            while (left <= right && sorted[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                swap(sorted, left, right);
+                left++; right--;
+            }
+        }
+        swap(sorted, leftIdx, right);
+        if (pivot != original[right]) {
+            answer++;
+        }
+        quickSort(sorted, leftIdx, right - 1);
+        quickSort(sorted, right + 1, rightIdx);
+    }
     
     public int heightChecker(int[] heights) {
         int[] sorted = heights.clone();
-        // quickSort(sorted, 0, sorted.length - 1);
+        answer = 0;
+        original = heights;
+        quickSort(sorted, 0, sorted.length - 1);
         // quickSort를 사용한 후, partioning이 수행될 때 마다 heights 배열과 비교하면 더 빨리 답을 구할 수 있을 것.
-        Arrays.sort(sorted);
-        int answer = getDiffCount(heights, sorted);
         return answer;
     }
 }
