eae71004bd43802bec83b14d038d07a4bf9363b0
kjy0349 <41959969+kjy0349@users.noreply.github.com>
2024-06-28 08:12:46 +0900

    Time: 0 ms (100.00%), Space: 66.6 MB (79.08%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,22 +1,8 @@
 import java.util.*;
 class Solution {
-    public static void putToMap(Map<Integer, Integer> target, int node) {
-        if (target.containsKey(node)) {
-            target.put(node, target.get(node) + 1);
-        } else {
-            target.put(node, 1);
-        }
-    }
     public int findCenter(int[][] edges) {
-        Map<Integer, Integer> edgeMap = new HashMap<>();
-        for (int[] edge : edges) {
-            int start = edge[0];
-            int end = edge[1];
-            putToMap(edgeMap, start);
-            putToMap(edgeMap, end);
-        }
-        TreeSet<Map.Entry<Integer, Integer>> sortedSet = new TreeSet<>((o1, o2) -> { return -Integer.compare(o1.getValue(), o2.getValue());});
-        sortedSet.addAll(edgeMap.entrySet());
-        return sortedSet.first().getKey();
+        int[] firstEdge = edges[0];
+        int[] secondEdge = edges[1];
+        return (firstEdge[0] == secondEdge[0] || firstEdge[0] == secondEdge[1]) ? firstEdge[0] : firstEdge[1];
     }
 }
